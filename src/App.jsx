97f85da04133f7df9d540910f2609e6d6cf587b5
Enis Tufekci <enistfkcii@gmail.com>
2023-11-17 16:05:12 +0300

    onChange ve map kavramlar

--- src/App.jsx
+++ src/App.jsx
@@ -12,17 +12,25 @@ function App() {
 //   setName("react degişti");
 // }
 
-const [count,setCount] = useState(0)
+// const [count,setCount] = useState(0)
 // const decrement = () => {
 //   if(!count < 1){
 //   // setCount(prev => prev - 1) // prev yazmak zorunda değiliz ancak bunun işlevi bi önceki kullandıgımız degeri alıp kullanmak.
 //   setCount(count - 1)
 //   }
 // }
+const [name,setName] = useState("")
+const [data,setData] = useState([])
+console.log(name)
 
-
-
-
+const clickFunc = () => {
+setData(prev => ([...prev,name]))
+setName('');
+}
+const targetFunc = (e) => {
+  setName(e.target.value)
+}
+console.log(data)
   return (
     // <>
     //   <Text1 number={"1"} name={"Enis"}></Text1> 
@@ -31,15 +39,26 @@ const [count,setCount] = useState(0)
     //   <Button name={"Artır"}></Button>
     //   <Button name={"Azalt"}></Button>
     // </>
+    // <>
+    // {/* <div onClick={clickFunc}>
+    // {name}
+    // </div> */}
+    // {/* <Button name={"Azalt"} onClick={() => setCount(count - 1)}></Button> */}
+    // {/* <button onClick={decrement}>Azalt</button> */}
+    // {/* <div>{count}</div>
+    // <Button name={"Artır"} onClick={() => setCount(count + 1)}></Button> */}
+    // {/* <button onClick={() => setCount(count+1)}>Artır</button> */}
+    // </>
     <>
-    {/* <div onClick={clickFunc}>
-    {name}
-    </div> */}
-    <Button name={"Azalt"} onClick={() => setCount(count - 1)}></Button>
-    {/* <button onClick={decrement}>Azalt</button> */}
-    <div>{count}</div>
-    <Button name={"Artır"} onClick={() => setCount(count + 1)}></Button>
-    {/* <button onClick={() => setCount(count+1)}>Artır</button> */}
+    <input value={name} type="text" onChange={targetFunc}></input>
+    <button onClick={clickFunc}>TIKLA</button>
+    <div>  
+    {
+    data.map((dt,i) => (
+      <div key={i}>{dt}</div>
+    ))
+    }
+    </div>
     </>
   )
 }
